42e68d508f9e90464579269ad0c77ac7c3cc3be9
Cheah Eng Teong <ceteongvanness@hotmail.com>
2020-06-17 10:03:18 +0800

    Arrays - Two Number Sum

---
+++ Arrays/1. Two Number Sum/Solution3.ts
@@ -0,0 +1,12 @@
+export function twoNumberSum(array: number[], targetSum: number) {
+    const nums: {[key: number]: boolean} = {};
+      for (const num of array){
+          const potentialMatch = targetSum - num;
+          if(potentialMatch in nums){
+              return [potentialMatch, num];
+          } else {
+              nums[num] = true;
+          }
+      }
+      return [];
+  }
