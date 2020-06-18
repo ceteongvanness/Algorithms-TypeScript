4a4188e67ab0e7aa6f956e30c385cc0118fbdc7f
Cheah Eng Teong <ceteongvanness@hotmail.com>
2020-06-18 15:44:40 +0800

    Binary Search Trees - Validate BST

---
+++ Binary Search Trees/3. Validate BST/Solution.ts
@@ -0,0 +1,24 @@
+class BST {
+    value: number;
+    left: BST | null;
+    right: BST | null;
+  
+    constructor(value: number) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  }
+  
+  export function validateBst(tree: BST) {
+    return validateBstHelper(tree, -Infinity, Infinity);
+  }
+  
+  function validateBstHelper(tree: BST | null, minValue: number, maxValue: number) : boolean{
+      if (tree === null) return true;
+      if (tree.value < minValue || tree.value >= maxValue) return false;
+      const leftIsValid = validateBstHelper(tree.left, minValue, tree.value);
+      return leftIsValid && validateBstHelper(tree.right, tree.value, maxValue);
+  }
+  
+  
