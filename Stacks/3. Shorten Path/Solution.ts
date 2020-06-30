914b8ccdbfc9012a85b33bff2f6e907fa759f17e
Cheah Eng Teong <ceteongvanness@hotmail.com>
2020-06-30 10:15:04 +0800

    Stacks - Shorten Path

---
+++ Stacks/3. Shorten Path/Solution.ts
@@ -0,0 +1,26 @@
+export function shortenPath(path: string) {
+    const startsWithSlash = path[0] === '/';
+      const tokens = path.split('/').filter(isImportantToken);
+      const stack: string[] = [];
+      if (startsWithSlash) stack.push('');
+      for (const token of tokens){
+          if (token === '..'){
+              if (stack.length === 0 || stack[stack.length - 1] === '..'){
+                  stack.push(token);
+              } else if (stack[stack.length - 1] !== ''){
+                  stack.pop();
+              }
+          } else {
+              stack.push(token);
+          }
+      }
+      
+      if (stack.length === 1 && stack[0] === '') return '/';
+      return stack.join('/');
+  }
+  
+  function isImportantToken(token: string){
+      return token.length > 0 && token !== '.';
+  }
+  
+  
