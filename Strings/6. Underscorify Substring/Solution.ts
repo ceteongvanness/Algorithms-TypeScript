e4a92d5095b64b6e215425c97f3435b696ca7e90
Cheah Eng Teong <ceteongvanness@hotmail.com>
2020-06-30 15:01:11 +0800

    Strings - Underscorify Substring

---
+++ Strings/6. Underscorify Substring/Solution.ts
@@ -0,0 +1,62 @@
+export function underscorifySubstring(string: string, substring: string) {
+    const locations = collapse(getLocations(string, substring));
+      return underscorify(string, locations);
+  }
+  
+  function getLocations(string: string, substring: string){
+      const locations: number[][] = [];
+      let startIdx = 0;
+      while (startIdx < string.length){
+          const nextIdx = string.indexOf(substring, startIdx);
+          if (nextIdx !== -1){
+              locations.push([nextIdx, nextIdx + substring.length]);
+              startIdx = nextIdx + 1;
+          } else {
+              break;
+          }
+      }
+      return locations;
+  }
+  
+  function collapse(locations: number[][]){
+      if (!locations.length) return locations;
+      const newLocations = [locations[0]];
+      let previous = newLocations[0];
+      for (let i = 1; i < locations.length; i++){
+          const current = locations[i];
+          if (current[0] <= previous[1]){
+              previous[1] = current[1];
+          } else {
+              newLocations.push(current);
+              previous = current;
+          }
+      }
+      return newLocations;
+  }
+  
+  function underscorify(string: string, locations: number[][]){
+      let locationsIdx = 0;
+      let stringIdx = 0;
+      let inBetweenUnderscores = false;
+      const finalChars = [];
+      let i = 0;
+      while (stringIdx < string.length && locationsIdx < locations.length){
+          if (stringIdx === locations[locationsIdx][i]){
+              finalChars.push('_');
+              inBetweenUnderscores = !inBetweenUnderscores;
+              if(!inBetweenUnderscores) locationsIdx++;
+              i = i === 1 ? 0 : 1;
+          }
+          finalChars.push(string[stringIdx]);
+          stringIdx++;
+      }
+      if (locationsIdx < locations.length){
+          finalChars.push('_');
+      } else if (stringIdx < string.length){
+          finalChars.push(string.slice(stringIdx));
+      }
+      return finalChars.join('');
+  }
+
+  
+  
